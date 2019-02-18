99c938a455e18369e20a29041d23aa571a81c112
Matias <matiass.hirvonen@gmail.com>
2019-02-18 12:26:57 +0200

    text box poistettu

--- Tulostaulu/tulostauluNaytto.Designer.cs
+++ Tulostaulu/tulostauluNaytto.Designer.cs
@@ -28,28 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.labelT1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.label12 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // labelT1
-            // 
-            this.labelT1.AutoSize = true;
-            this.labelT1.Location = new System.Drawing.Point(191, 214);
-            this.labelT1.Name = "labelT1";
-            this.labelT1.Size = new System.Drawing.Size(46, 17);
-            this.labelT1.TabIndex = 0;
-            this.labelT1.Text = "label1";
-            this.labelT1.Click += new System.EventHandler(this.labelT1_Click);
-            // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(316, 58);
+            this.pictureBox1.Location = new System.Drawing.Point(260, 58);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 86);
             this.pictureBox1.TabIndex = 1;
@@ -57,55 +44,29 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(422, 58);
+            this.pictureBox2.Location = new System.Drawing.Point(441, 58);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(100, 86);
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(369, 214);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(46, 17);
-            this.label12.TabIndex = 3;
-            this.label12.Text = "label1";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(649, 279);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // tulostauluNaytto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label12);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.labelT1);
             this.Name = "tulostauluNaytto";
             this.Text = "tulostauluNaytto";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label labelT1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Button button1;
     }
 }
