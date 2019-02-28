b9b7780a23b5b5a2104d440e03f6ccffa136eace
Mikael Polojärvi <mikaelpo@student.uef.fi>
2019-02-28 18:27:40 +0200

    testi

--- Tulostaulu/tulostauluNaytto.Designer.cs
+++ Tulostaulu/tulostauluNaytto.Designer.cs
@@ -124,7 +124,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(85, 79);
+            this.label5.Location = new System.Drawing.Point(82, 79);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(46, 17);
             this.label5.TabIndex = 10;
