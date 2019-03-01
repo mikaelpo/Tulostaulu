7e1c71d82f7578efa49f8f8c15bd899ebb633831
Aleksi Hirvonen <alekshi@uef.fi>
2019-03-01 12:53:47 +0200

    seuraava button

--- Tulostaulu/asetukset.cs
+++ Tulostaulu/asetukset.cs
@@ -22,20 +22,21 @@ namespace Tulostaulu
         private void buttonA1_Click(object sender, EventArgs e)
         {
 
-            a1.setNeljanneksenpituus(Convert.ToInt32(textBox1));
-            a1.setNeljanneksienmaara(Convert.ToInt32(textBox2));
-            a1.setJatkoajanpituus(Convert.Toint32(textBox3));
-            a1.setLyhyttauko(convert.Toint32(textBox1));
-            a1. neljanneksenPituus(convert.Toint32(textBox1));
-            a1. neljanneksenPituus(convert.Toint32(textBox1));
-            a1. neljanneksenPituus(convert.Toint32(textBox1));
-            a1. neljanneksenPituus(convert.Toint32(textBox1));
-            a1. neljanneksenPituus(convert.Toint32(textBox1));
-            a1. neljanneksenPituus(convert.Toint32(textBox1));
-            a1. neljanneksenPituus(convert.Toint32(textBox1));
+            a1.setNeljanneksenpituus(Convert.ToInt32(textBox1.Text));
+            a1.setNeljanneksienmaara(Convert.ToInt32(textBox2.Text));
+            a1.setJatkoajanpituus(Convert.ToInt32(textBox3.Text));
+            a1.setLyhyttauko(Convert.ToInt32(textBox5.Text));
+            a1.setPitkatauko(Convert.ToInt32(textBox7.Text));
+            a1.setAikalisa(Convert.ToInt32(textBox6.Text));
+            a1.setAikalisienlukumaara(Convert.ToInt32(textBox8.Text));
+            a1.setHyokkaysaika1(Convert.ToInt32(textBox9.Text));
+            a1.setHyokkaysaika1(Convert.ToInt32(textBox10.Text));
+            a1.setVirheet(Convert.ToInt32(textBox11.Text));
+            a1.setPelikellonsummeri(Convert.ToInt32(textBox13.Text));
+            a1.setHeittokellonsummeri(Convert.ToInt32(textBox12.Text));
             
-           
-           
+
+
             kokoonpanot k1 = new kokoonpanot(a1);
             k1.Show();
             Close();
