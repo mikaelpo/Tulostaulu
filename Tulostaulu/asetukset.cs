6418024dc415c314db2b499e8f2dcf0e7b9a4c56
Matias <matiass.hirvonen@gmail.com>
2019-03-01 12:03:05 +0200

    oletus asetukset painike päivitetty

--- Tulostaulu/asetukset.cs
+++ Tulostaulu/asetukset.cs
@@ -32,5 +32,23 @@ namespace Tulostaulu
             
             a1.setNeljanneksienmaara(Convert.ToInt32(textBox2.Text));
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = 10.ToString();
+            textBox2.Text = 4.ToString();
+            textBox3.Text = 5.ToString();
+            textBox5.Text = 2.ToString();
+            textBox7.Text = 15.ToString();
+            a1.setKellonkayntisuuntaalaspain(true);
+            textBox6.Text = 60.ToString();
+            textBox8.Text = 3.ToString();
+            textBox9.Text = 24.ToString();
+            textBox10.Text = 14.ToString();
+            textBox11.Text = 5.ToString();
+            textBox13.Text = 2.ToString();
+            textBox12.Text = 2.ToString();
+
+        }
     }
 }
